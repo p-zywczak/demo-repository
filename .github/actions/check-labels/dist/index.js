5794bccf9057ee67d8797714e5b7f6ec3949d654
Piotr <pzywczak@remedia-it.pl>
2025-03-20 12:26:32 +0100

    refresh

--- .github/actions/check-labels/dist/index.js
+++ .github/actions/check-labels/dist/index.js
@@ -29915,12 +29915,46 @@ function wrappy (fn, cb) {
 /***/ }),
 
 /***/ 1134:
-/***/ ((__unused_webpack_module, exports) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.LabelChecker = void 0;
+const core = __importStar(__nccwpck_require__(7484));
 class LabelChecker {
     constructor(githubApi, context) {
         this.githubApi = githubApi;
@@ -29937,11 +29971,21 @@ class LabelChecker {
     }
     async verifyRequiredLabels(required) {
         const labelsNames = await this.fetchLabelsOnPR();
-        const missingLabels = required.filter(label => !labelsNames.includes(label));
-        if (missingLabels.length > 0) {
-            const errorMessage = `PR nie ma wymaganych etykiet: ${missingLabels.join(', ')}.`;
-            throw new Error(errorMessage);
-        }
+        required.forEach(label => {
+            if (!labelsNames.includes(label)) {
+                core.setFailed(`PR nie ma wymaganej etykiety ${label}.`);
+            }
+        });
+    }
+    async verifyAnyOfLabels(anyOfLabelsGroups) {
+        const labelsNames = await this.fetchLabelsOnPR();
+        anyOfLabelsGroups.forEach((group) => {
+            const hasLabel = labelsNames.some(label => group.includes(label));
+            if (!hasLabel) {
+                const groupStr = `[${group.join(', ')}]`;
+                core.setFailed((`Brakuje labelek z grupy: ${groupStr}.`));
+            }
+        });
     }
 }
 exports.LabelChecker = LabelChecker;
@@ -30005,15 +30049,8 @@ async function run() {
         branchName: (_c = github.context.payload.pull_request) === null || _c === void 0 ? void 0 : _c.head.ref,
     };
     const labelChecker = new LabelChecker_1.LabelChecker(githubApi, context);
-    const labels = await labelChecker.fetchLabelsOnPR();
-    core.info(`🔍 Sprawdzam przynajmniej labelki na PR: ${labels}`);
-    try {
-        await labelChecker.verifyRequiredLabels(requiredLabels);
-    }
-    catch (error) {
-        const errorMessage = error.message;
-        core.setFailed(errorMessage);
-    }
+    await labelChecker.verifyRequiredLabels(requiredLabels);
+    await labelChecker.verifyAnyOfLabels(anyOfLabels);
     core.info('test');
 }
 run();
