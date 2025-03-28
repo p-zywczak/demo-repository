08ca9f5b70ac80996e41df8887251fe0aafbee14
Piotr <pzywczak@remedia-it.pl>
2025-03-28 15:03:45 +0100

    refresh

---
+++ .github/actions/release-branch-synchronizer/src/main.ts
@@ -0,0 +1,18 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { ReleaseBranchSynchronizer } from './ReleaseBranchSynchronizer'
+async function run(): Promise<void> {
+    const githubApi= github.getOctokit(core.getInput('token'));
+    const ver = core.getInput('ver', );
+    const repoOwner = core.getInput('repo_owner');
+    const repoName = core.getInput('repo_name');
+
+    const releaseBranchSynchronizer:ReleaseBranchSynchronizer = new ReleaseBranchSynchronizer(githubApi, ver, repoOwner, repoName);
+    if(await releaseBranchSynchronizer.checkReleaseBranchExists()) {
+        process.exit(0);
+    } else {
+
+    }
+}
+
+run();
