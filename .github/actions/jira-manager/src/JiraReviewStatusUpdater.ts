129bb72846015fc58f975fed0e584fd0b2797ecb
Piotr <pzywczak@remedia-it.pl>
2025-04-08 13:58:37 +0200

    jira

--- .github/actions/jira-manager/src/JiraReviewStatusUpdater.ts
+++ .github/actions/jira-manager/src/JiraReviewStatusUpdater.ts
@@ -1,12 +1,16 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { Version3Client } from 'jira.js'
 
 export class JiraReviewStatusUpdater {
     protected client: Version3Client;
+    protected githubApi;
+    protected context = github.context;
 
     constructor(
         private readonly email:string,
         private readonly token:string,
+        private readonly githubToken:string,
         private readonly url:string,
         private readonly projectId:string,
         private readonly environment:string,
@@ -20,5 +24,21 @@ export class JiraReviewStatusUpdater {
                 }
             }
         });
+        this.githubApi = github.getOctokit(githubToken);
+    }
+    public async handle(){
+        const labels = this.fetchLabelsOnPR();
+        core.info(`Labels : ${labels}`);
+    }
+    private async fetchLabelsOnPR(): Promise<any>
+    {
+        const {owner, repo} = this.context.repo
+        const prNumber:number = this.context.payload.pull_request?.number!;
+        const { data } = await this.githubApi.rest.issues.listLabelsOnIssue({
+            owner,
+            repo,
+            issue_number: prNumber,
+        });
+        return data.map(label => label.name);
     }
 }
--- .github/actions/jira-manager/src/main.ts
+++ .github/actions/jira-manager/src/main.ts
@@ -2,14 +2,17 @@ import * as core from '@actions/core';
 import {JiraCreateRelease} from "./JiraCreateRelease";
 import {OperationTypeEnum} from "./OperationTypeEnum";
 import {JiraMarkRelease} from "./JiraMarkRelease";
+import {JiraReviewStatusUpdater} from "./JiraReviewStatusUpdater";
 
 async function run(): Promise<void> {
     const email:string = core.getInput('jira_email');
     const token:string = core.getInput('jira_token');
+    const github_token:string = core.getInput('github_token');
     const url:string = core.getInput('jira_url');
     const projectId:string = core.getInput('jira_project_id');
     const environment:string = core.getInput('environment');
     const idAwaitingToTesting:string = core.getInput('jira_id_awaiting_to_testing');
+    const idCodeReviewDone:string = core.getInput('jira_id_code_review_done');
     const githubRef:string = core.getInput('github_ref');
     const commitMessage:string = core.getInput('commit_message');
     const type:string = core.getInput('type') as OperationTypeEnum;
@@ -29,6 +32,8 @@ async function run(): Promise<void> {
             await jiraMark.releaseVersion();
             break;
         case (OperationTypeEnum.ReviewStatusUpdater):
+            const jiraReview:JiraReviewStatusUpdater = new JiraReviewStatusUpdater(email, token, github_token, url, projectId, environment);
+            await jiraReview.handle();
             break;
         default:
             core.setFailed('Unknown operation type');
